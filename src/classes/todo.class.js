9de32c1c8488a80b21f04944fc19578298d28dff
Alejo <calvi.alejo@gmail.com>
2022-03-21 15:25:49 -0300

    Primer commit

---
+++ src/classes/todo.class.js
@@ -0,0 +1,30 @@
+
+
+export class Todo {
+
+    static fromJson({ id, task, complete, created }) {
+
+        const tempTodo = new Todo( task );
+        
+        tempTodo.id = id;
+        tempTodo.complete = complete;
+        tempTodo.created = created;
+
+        return tempTodo;
+    }
+
+    constructor( task ) {
+
+        this.task       = task;
+        this.id         = new Date().getTime();
+        this.complete   = false;
+        this.created    = new Date(); 
+
+    }
+
+    imprimirClase(){
+        console.log(`${ this.tarea } - ${ this.id }`);
+    }
+
+
+}
